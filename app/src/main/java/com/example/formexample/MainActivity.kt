450f5ceca2b0f5ef759183abfab9aa9101d45448
thekill022 <gibfathoni@gmail.com>
2025-11-07 09:15:12 +0700

    memanggil fungsi di scaffold

--- app/src/main/java/com/example/formexample/MainActivity.kt
+++ app/src/main/java/com/example/formexample/MainActivity.kt
@@ -3,26 +3,29 @@ package com.example.formexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.formexample.ui.theme.FormExampleTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FormExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold {
+                    innerPadding ->
+                        Form(modifier = Modifier.fillMaxSize().background(color = Color.Blue).padding(innerPadding))
                 }
             }
         }
