0d337513221a3c5847e5614948d80d9e4d374194
thekill022 <gibfathoni@gmail.com>
2025-11-07 09:09:17 +0700

    membuat tata letak column sebagai pembungkus utama content

--- app/src/main/java/com/example/formexample/form.kt
+++ app/src/main/java/com/example/formexample/form.kt
@@ -1,8 +1,12 @@
 package com.example.formexample
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun Form() {
+fun Form(modifier : Modifier) {
+    Column(modifier = modifier) {
 
+    }
 }
